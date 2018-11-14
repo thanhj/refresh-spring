800373e8e4d7ea11608d02a8f85a6cac57451018
Thanh Nguyen Ky <thanh.nguyenky@singtel.com>
2018-11-14 23:25:27 +0800

    first commit

---
+++ src/main/java/my/thanh/refreshspring/RefreshSpringApplication.java
@@ -0,0 +1,12 @@
+package my.thanh.refreshspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshSpringApplication.class, args);
+	}
+}
